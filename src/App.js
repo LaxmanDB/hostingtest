2bf190df9d5a0137e22a66ff41e6c7c3709cbcc6
LaxmanDB <bhosalelaxman2020@yahoo.com>
2021-11-17 10:08:56 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Laxman testing CI/CD
+          Laxman bhosale
         </p>
         <a
           className="App-link"
